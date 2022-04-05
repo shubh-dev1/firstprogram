ad6bbeb081054ec7f38f6f8e4269d4df8c997236
Shubham <Singhalshubham610@gmail.com>
2022-04-05 14:30:11 +0530

    initialcommit

---
+++ addTwoNumber.java
@@ -0,0 +1,15 @@
+public class addTwoNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+         
+        int c = a+b; // here we are taking extra varible
+
+        System.out.println(c);
+
+
+        // print direct without extra variable
+        System.out.println(a+b);
+    }
+    
+}
